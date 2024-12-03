b134e962fb4a524c709e2900f221b23b82a6be53
MaheshKafle <mahesh.kafle@lisec.com>
2024-12-04 01:32:54 +0400

    Added Test case for Auto Suggestive Dropdowns like Google search bar

---
+++ src/AutoSuggestiveDropdown.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutoSuggestiveDropdown 
+{
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		// 1. Running in Chrome browser
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaflemah\\Personal\\Drivers\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		//Maximize the window
+		driver.manage().window().maximize();
+		driver.findElement(By.id("autosuggest")).sendKeys("ind");
+		Thread.sleep(3000);
+		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+		for(WebElement option : options)
+		{
+			if(option.getText().equalsIgnoreCase("India"))
+			{
+				option.click();
+			}
+		}
+		
+	}
+}
