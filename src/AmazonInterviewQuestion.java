fbc76e69fd22ef4a7c90a2d0af079218013ace5e
MaheshKafle <mahesh.kafle@lisec.com>
2024-12-09 01:08:08 +0400

    Added implicit waits, explicit wait and Thread.sleep concepts in existing add to cart ecommerce website

--- src/AmazonInterviewQuestion.java
+++ src/AmazonInterviewQuestion.java
@@ -1,9 +1,14 @@
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class AmazonInterviewQuestion 
@@ -13,22 +18,38 @@ public class AmazonInterviewQuestion
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaflemah\\Personal\\Drivers\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.get("https://rahulshettyacademy.com/seleniumPractise");
 		//Maximize the window
 		driver.manage().window().maximize();
-		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-		
 		
 		String[] itemsNeeded = {"Brocolli","Cucumber","Beetroot", "Brinjal"};
-
+		addItems(driver, itemsNeeded);
+		
+		driver.findElement(By.cssSelector(".cart-icon")).click();
+		driver.findElement(By.xpath("//button[contains(text(), 'PROCEED TO CHECKOUT')]")).click();
+		driver.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector(".promoBtn")).click();
+		//Explicit wait
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+		
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText()); 
+		driver.findElement(By.xpath("//button[contains(text(), 'Place Order')]")).click();
+		
+	}
+	
+	public static void addItems(WebDriver driver, String[] itemsNeeded)
+	{
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 		//Here we convert itemsNeeded List to ArrayList as search in ArrayList is very easy using .contains method which List doesn't support
         List itemsNeededArrayList = Arrays.asList(itemsNeeded);
-       
+        int j = 0;
 		for(int i=0; i<products.size(); i++) 
 		{	
 			String[] name = products.get(i).getText().split("-");
 			String formattedName = name[0].trim();
-			int j = 0;
+			
 			
 			//Search for products from ArrayList and click on that particular product and once it clicks the final item then break the program
 			if(itemsNeededArrayList.contains(formattedName))
@@ -46,7 +67,6 @@ public class AmazonInterviewQuestion
 				
 			}
 		}
-		
 	}
 
 }
